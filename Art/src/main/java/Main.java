ebdf2d33d18572c2328ed4c0eb0b04641f8dd339
Victor <1lemanruss1@gmail.com>
2019-09-10 21:46:25 +0300

    add trash

---
+++ Art/src/main/java/Main.java
@@ -0,0 +1,37 @@
+
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //       System.out.println("test");
+        JFrame window = new JFrame();
+        JFileChooser fileChooser = new JFileChooser();
+        File selectedFile = null;
+        int returnVal;
+       do {     returnVal =  fileChooser.showOpenDialog(null);
+        //System.out.println("Selected file: " + fileChooser.getSelectedFile().getAbsolutePath());
+            selectedFile = fileChooser.getSelectedFile();
+           try {
+               Thread.sleep(50);
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }while((returnVal != JFileChooser.APPROVE_OPTION)||(!fileChooser.getSelectedFile().exists()));
+        try {
+            Scanner filescanner = new Scanner(selectedFile);
+            while(filescanner.hasNext()){
+                System.out.println(filescanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+//плеыер - биграмный шифр тк ассматриваются 2 сисвола - надо генерировать подстановочную таблицу размер которой равен ол-ву букв алфавита например для англа 5 на 5 = 25 а ваще найс - ключ в начале таблички а оставшиеся дальше тк шифр биграмны надо рассматривать2 символа сначала текст разбивается по парам и буквы шифруются исходя из своей позиции а 2 одинаковые подряд разбиваются например иксом как редкими если текст нечетной длины то добавить вконец че-то еще например тот же символ
+//
