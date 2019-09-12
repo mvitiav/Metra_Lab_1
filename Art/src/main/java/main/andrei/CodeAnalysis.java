7a139adb1d36fe5172efd0abe8dfd76dd20f5e3f
Andrei <shakovskiyandrei@gmail.com>
2019-09-12 19:49:03 +0300

    Откат

--- Art/src/main/java/main/andrei/CodeAnalysis.java
+++ Art/src/main/java/main/andrei/CodeAnalysis.java
@@ -6,20 +6,17 @@ import java.util.regex.Pattern;
 public class CodeAnalysis {
     public final String[] TYPES_LIST = {"byte", "short", "int", "long", "float", "double", "char", "boolean", "String", "class", "void", "interface"};
     public final String[] AFTER_NAME_SIGNS = {";", "=", "{", "(", ")", ","};//скобка, если метод
-    public String getRegisteredOperands(String text){
-        String registeredOperands = "";
-        Pattern pattern = Pattern.compile("\\b(byte|short|int|long|float|double|char|boolean|String|class|void|interface)\\b.+?[{;(),=]");
-        Matcher matcher = pattern.matcher(text);
-        while (matcher.find()) {
-            System.out.println(text.substring(matcher.start(), matcher.end()));
-        }
-        return registeredOperands;
+    public String getRegisteredOperators(String text){
+        String registeredOperators = "";
+
+        return registeredOperators;
     }
 
     //метод для выпиливания строковых операндов
     public String getStrings(String text){
         String strings = "";
-        //char nullChar = 0;
+        char nullChar = 0;
+        // не тот Pattern pattern = Pattern.compile("\\b(byte|short|int|long|float|double|char|boolean|String|class|void|interface)\\b.+?[{;(),=]");
         Pattern pattern = Pattern.compile("\".+?\"");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
@@ -29,3 +26,93 @@ public class CodeAnalysis {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Пускай тут полежит пока
+public class CodeAnalysis {
+    public final String[] TYPES_LIST = {"byte", "short", "int", "long", "float", "double", "char", "boolean", "String", "class", "void", "interface"};
+    public final String[] AFTER_NAME_SIGNS = {";", "=", "{", "(", ")", ","};//скобка, если метод
+    public String getRegisteredOperators(String[] codeLines){
+        String registeredOperators = "";
+        for (int curType = 0; curType < TYPES_LIST.length; curType++){
+            for (int curLine = 0; curLine < codeLines.length; curLine++){ //берем одну строку
+                if (codeLines[curLine].indexOf(TYPES_LIST[curType]) != -1){
+                    //если в анализируемой строке есть объявление типа, класса, метода...
+                    // ... то после имени должно быть равно (+инициализация), точка с запятой или скобка (для метода и класса)
+                    String sign = "!";
+                    int minPos = codeLines[curLine].length()+1; //положение ближайшего "нужного" символа
+                    for (int curSign = 0; curSign < AFTER_NAME_SIGNS.length; curSign++){
+                        if (codeLines[curLine].indexOf(AFTER_NAME_SIGNS[curSign]) != -1){//если в строке есть-таки "нужный" знак ...
+                            if ((codeLines[curLine].indexOf(AFTER_NAME_SIGNS[curSign]) < minPos) && codeLines[curLine].indexOf(AFTER_NAME_SIGNS[curSign]) > codeLines[curLine].indexOf(TYPES_LIST[curType])){ //... то чекаем, какой ближе
+                                minPos = codeLines[curLine].indexOf(AFTER_NAME_SIGNS[curSign]);
+                                sign = AFTER_NAME_SIGNS[curSign];
+                            }
+                        }
+                    }
+
+                    if (!sign.equals("!")){
+                        registeredOperators += codeLines[curLine].substring(codeLines[curLine].indexOf(TYPES_LIST[curType])+TYPES_LIST[curType].length(), codeLines[curLine].indexOf(sign)).trim() + ";";
+                    }
+                    else {
+                        System.out.println("Что-то пошло не так!");
+                        System.out.println("Чекнуть строку : " + codeLines[curLine]);
+                        System.out.println("{ pos" + codeLines[curLine].indexOf("{"));
+                        System.out.println("( pos" + codeLines[curLine].indexOf("("));
+                        System.out.println("; pos" + codeLines[curLine].indexOf(";"));
+                        System.out.println("= pos" + codeLines[curLine].indexOf("="));
+                        System.out.println("len = " + codeLines[curLine].length());
+                        System.out.println("sign = " + sign);
+                        System.out.println("minPos = " + minPos);
+                        System.out.println("curType = " + TYPES_LIST[curType]);
+                        System.out.println("FROM = " + codeLines[curLine].indexOf(TYPES_LIST[curType])+TYPES_LIST[curType].length());
+                        System.out.println("TO = " +  codeLines[curLine].indexOf(sign));
+                    }
+                }
+            }
+        }
+        return registeredOperators;
+    }
+}
+ */
