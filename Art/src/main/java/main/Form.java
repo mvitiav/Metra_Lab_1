ff799fecc278bd1491afd837e4911cd99b94f74e
Victor <1lemanruss1@gmail.com>
2019-09-12 21:51:40 +0300

    Add Infopanel and other improvenents to Form

--- Art/src/main/java/main/Form.java
+++ Art/src/main/java/main/Form.java
@@ -1,8 +1,12 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;import java.awt.event.ComponentAdapter;
+import java.io.File;
+import java.io.IOException;
 
 import static main.andrei.Main.buttonClicked;
 
@@ -11,6 +15,8 @@ public class Form extends JFrame{
     private JButton chooseFileButton;
     private JTable table1;
     private JPanel panel1;
+    private JLabel Operators;
+    private JPanel InfoPanel;
     private MyTableModel tableModel=null;
 
     public MyTableModel getTableModel() {
@@ -24,6 +30,12 @@ public class Form extends JFrame{
     public Form() {
         tableModel=new MyTableModel();
         table1.setModel(tableModel);
+        try {
+            chooseFileButton.setFont(Font.createFont(Font.TRUETYPE_FONT, new File("Art/src/main/resources/Red October.ttf")).deriveFont(14f));
+        } catch (FontFormatException | IOException e) {
+         JOptionPane.showMessageDialog(null,"что - то отвалилося((");
+            e.printStackTrace();
+        }
         setSize(800, 600);
         setTitle("Моднейшее окно!");
         setLocationRelativeTo(null);
