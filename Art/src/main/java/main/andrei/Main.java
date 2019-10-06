059fdb0566791f8ffacfbdb77da93cda183a1d13
Andrei <shakovskiyandrei@gmail.com>
2019-10-06 15:04:17 +0300

    Check it.

--- Art/src/main/java/main/andrei/Main.java
+++ Art/src/main/java/main/andrei/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
     public static void main (String[] args) {
 
+        if (true){}//TODO: не пушило
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
